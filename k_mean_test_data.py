9da2288e08430ee62d1e2cdabf0ecf5b1b29c8c3
Danny Thanakrit Boonquarmdee <pb.danny@gmail.com>
2018-05-22 19:50:40 +0700

    add random centroid

--- k_mean_test_data.py
+++ k_mean_test_data.py
@@ -14,6 +14,7 @@ K-mean clustering from therory
 4. Calculate new centroid from avg distance of each k cluster
 5. Back to 2. until change in each cluster so minimal
 """
+import random
 import os
 import numpy as np
 import pandas as pd
@@ -30,3 +31,23 @@ df.plot.scatter(x = 'V1', y = 'V2', s = 1)
 # Euclidean distance
 def dist(df, centroid):
     return (df - centroid).pow(2).sum(axis = 'columns').pow(1/2)
+
+# random centroid
+def rand_centroid(df, k):
+    dim = df.ndim
+    list_cen = []
+    for i in range(k):
+        for j in range(dim):
+            rand_centroid = random.choice(df.iloc[:,j])
+            list_cen.append(rand_centroid)
+    out = pd.DataFrame(np.array(list_cen).reshape(k,dim),
+                      columns = df.columns)
+    return out
+
+# calculate distance each centroid
+def dist_centroids(df, centroids):
+    dim = df.ndim
+    list_dist = []
+    for centroid in centroids:
+         dist(df, centroid)
+
